704b59ce97bb517832493f489f4b183d6badfe8f
StarkillerX42 <dgatlin5@gmail.com>
2019-04-27 13:42:16 -0600

    Changed default volume and scalar method

--- alarm.py
+++ alarm.py
@@ -13,6 +13,7 @@ class AlarmClock:
     """An alarm clock for personal use, it will run through the songs for a
     desired amount of time, as given in an argument
     """
+
     def __init__(self):
         self.start = datetime.datetime.now()
         s.iprint("Alarm started at {}".format(self.start), 0)
@@ -23,15 +24,16 @@ class AlarmClock:
         np.random.shuffle(self.songs)
         s.iprint("There are {} songs".format(len(self.songs)), 1)
         self.played_weather = False
-        self.volume = 0.20
-        self.dvol = 0.01
+        self.volume_init = 0.20
+        self.volume_final = 0.35
+        self.volume = self.volume_init
 
     def play_song(self, song):
         song_data = sub.Popen('sox "{}" -n stat'.format(song), shell=True,
                               stderr=sub.PIPE).stderr.readlines()
         vol_lvl = float(song_data[6].split()[-1])
-        sub.call('play -q -v {} "{}"'.format(self.volume/vol_lvl, song),
-                  shell=True)
+        sub.call('play -q -v {} "{}"'.format(self.volume / vol_lvl, song),
+                 shell=True)
         now = datetime.datetime.now()
         dt = now - self.start
         return dt
@@ -41,12 +43,14 @@ class AlarmClock:
             s.iprint("Playing {}".format(song), 1)
             dt = self.play_song(song)
 
-            if dt.seconds >= 30*60:
+            if dt.seconds >= 30 * 60:
                 if not self.played_weather:
                     self.play_weather()
                     self.played_weather = True
-                self.volume += self.dvol
-            if dt.seconds >= 1.5*3600:
+                self.volume = (self.volume_init + (self.volume_final
+                                                   - self.volume_init)
+                               * dt.seconds / 30 / 60)
+            if dt.seconds >= 1.5 * 3600:
                 sys.exit()
 
     def play_weather(self):

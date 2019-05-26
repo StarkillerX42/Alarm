323c5480a0d5fc244d5a45426cce1b89666b71e0
StarkillerX42 <dgatlin5@gmail.com>
2019-05-26 09:56:41 -0600

    Lowered maz volume to 14

--- alarm.py
+++ alarm.py
@@ -25,7 +25,7 @@ class AlarmClock:
         s.iprint("There are {} songs".format(len(self.songs)), 1)
         self.played_weather = False
         self.volume_init = 0.05
-        self.volume_final = 0.15
+        self.volume_final = 0.14
         self.volume = self.volume_init
 
     def play_song(self, song):

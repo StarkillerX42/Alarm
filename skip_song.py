ae1aec312f29634ff17c5dd56338b025075d6065
Dylan Gatlin <dgatlin5@gmail.com>
2019-07-20 08:33:13 -0600

    Added song skip routine

--- skip_song.py
+++ skip_song.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
-import os
-import sys
+import subprocess as sub
+sub.call('ps auxf > procs.txt')
+with open("procs.txt", "r") as proc_fil:
+    lines = np.array(proc_fil.readlines())
+is_song = np.array(["play -q -v" in proc for proc in lines])
+to_kill = lines[is_song]
+for proc in to_kill:
+    # print(proc)
+    id = proc.split()[1]
+    # print(id)
+    sub.call(f'kill {id}')
+sub.call(['rm', 'procs.txt'])
 

ba3b0d767875214a0784945dd17a1dcff8596b45
Dylan Gatlin <dgatlin5@gmail.com>
2019-10-27 12:56:10 -0600

    Adding argparse to code

--- alarm.py
+++ alarm.py
@@ -1,8 +1,9 @@
-#!/home/pi/berryconda3/bin/python
+#!/usr/bin/env python3
 import numpy as np
 import datetime
 import time
 import sys
+import argparse
 import subprocess as sub
 from pathlib import Path
 import starcoder42 as s
@@ -14,28 +15,41 @@ class AlarmClock:
     desired amount of time, as given in an argument
     """
 
-    def __init__(self):
+    def __init__(self, flacs, wavs, mp3s, screensaver=False, cec=False):
+        self.flacs = flacs
+        self.wavs = wavs
+        self.mp3s = mp3s
+        self.screensaver = screensaver
+        self.cec = cec
+
         self.start = datetime.datetime.now()
         s.iprint("Alarm started at {}".format(self.start), 0)
-        tv_on = sub.call('echo "on 0" | cec-client -s -d 1', shell=True)
-        s.iprint(tv_on, 1)
-        tv_source = sub.call('echo "as" | cec-client -s -d 1 -p 1', shell=True)
-        s.iprint(tv_source, 1)
-        # Set volume to 16
-        # sub.call('echo "voldown 0" | cec-client -s -d 1 -p 1', shell=True)
-        # sub.call('echo "volup 16" | cec-client -s -d 1 -p 1', shell=True)
-        shodan = Path('/media/pi/SHODAN/Music/')
-        wavs = shodan.rglob('*.wav')
-        mp3s = shodan.rglob('*.wav')
-        flacs = Path('/media/pi/pi_red/flac/').rglob('*.flac')
-        self.songs = np.array(list(wavs) + list(mp3s) + list(flacs))
+        
+        sub.call('amixer set PCM -- 40%', shell=True)
+
+        if self.cec:
+            tv_on = sub.call('echo "on 0" | cec-client -s -d 1 -p 3',
+                             shell=True)
+            s.iprint(tv_on, 1)
+            time.sleep(20)
+            tv_source = sub.call('echo "as" | cec-client -s -d 1 -p 3',
+                                 shell=True)
+            s.iprint(tv_source, 1)
+            # Set volume to 16
+            # sub.call('echo "voldown 0" | cec-client -s -d 1 -p 1', shell=True)
+            # sub.call('echo "volup 16" | cec-client -s -d 1 -p 1', shell=True)
+        
+        self.songs = np.array(list(self.wavs)
+                              + list(self.mp3s)
+                              + list(self.flacs))
         np.random.shuffle(self.songs)
         s.iprint("There are {} songs".format(len(self.songs)), 1)
         self.played_weather = False
         self.volume_init = 0.055
         self.volume_final = 0.14
         self.volume = self.volume_init
-        sub.call('xscreensaver-command -activate', shell=True)
+        if self.screensaver:
+            sub.call('xscreensaver-command -activate', shell=True)
 
     def try_bluetooth(self):
         nearby_devices = bt.discover_devices(lookup_names=True)
@@ -47,6 +61,7 @@ class AlarmClock:
     def play_song(self, song):
         song_data = sub.Popen('sox "{}" -n stat'.format(song), shell=True,
                               stderr=sub.PIPE).stderr.readlines()
+        # print(song_data)
         vol_lvl = float(song_data[6].split()[-1])
         # print(self.volume/vol_lvl)
         res = sub.call('play -q -v {} "{}"'.format(self.volume/vol_lvl, song),
@@ -82,18 +97,38 @@ class AlarmClock:
 
     def play_weather(self):
         report = Weather()
+        report.convert-units()
         report.make_mp3()
         report.play_forecast()
         report.send_daily('beancc_weather.key')
 
     def stop(self):
-        sub.call('echo "standby 0" | cec-client -s -d 1', shell=True)
-        sub.call('xscreensaver-command -deactivate', shell=True)
-
+        if self.screensaver:
+            sub.call('xscreensaver-command -deactivate', shell=True) 
+        if self.cec:
+            sub.call('echo "standby 0" | cec-client -s -d 1 -p 3', shell=True)
+        
 
 
 def main():
-    alarm = AlarmClock()
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-p', '--paths', nargs='+', help='Paths with music in'
+                                                         ' them')
+    parser.add_argument('-s', '--screensaver', action='store_true',
+                            default=False, help='Whether or not to use'
+                            'xscreensaver during the alarm')
+    parser.add_argument('-c', '--cec', action='store_true', default=False,
+                        help='Whether or not to issue cec commands to a TV')
+    args = parser.parse_args()
+    flacs = []
+    wavs = []
+    mp3s = []
+    for path in args.paths:
+        flacs += list(Path(path).rglob('*.flac'))
+        wavs += list(Path(path).rglob('*.wav'))
+        mp3s += list(Path(path).rglob('*.mp3'))
+    alarm = AlarmClock(flacs, wavs, mp3s, screensaver=args.screensaver,
+                       cec=args.cec)
     alarm.run()
 
 
